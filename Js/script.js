022745bff6da3577cde1e1d877866547f62b4972
Neville Oronni <nevooronni@gmail.com>
2017-08-27 22:38:08 +0300

    added message functionality

--- Js/script.js
+++ Js/script.js
@@ -2,7 +2,11 @@
 
 //front-end 
 $(document).ready(function () {
-	$("i.glyphicon-plus").click(function() {
+	$("i.glyphicon-plus").click(function() {//add topic button functionality
     $("#addTopic").slideToggle();
+
+    //tooltips
+    $("[data-toggle='tooltip']").tooltip();
+		$("[data-toggle='tooltip']").tooltip();
 	});
 });
