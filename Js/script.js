54cc7c7c193d77c3e711cea1102ef9d666f40ecd
Zachnjue <njuezachary@gmail.com>
2017-08-27 19:07:51 +0300

    increased the width of the div

---
+++ Js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("#log").click(function(){
+    $("#unique").slideToggle();
+    
+
+  });
+  $("#sgnup").click(function(){
+    $("#unique2").slideToggle();
+  });
+});
